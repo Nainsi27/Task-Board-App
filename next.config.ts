f11f3b6c6eb6c1f6b839890e9f799a32d5783cea
Nainsi Pandey <115419812+Nainsi27@users.noreply.github.com>
2026-02-12 18:14:29 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // DO NOT include "output: 'export'" here
+  // Render needs server-side rendering, not static export
 };
 
 export default nextConfig;
